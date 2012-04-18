386283fb9cc1530dde9f72bbf699c5dee783727e
Zjamnik <zjamnik@gmail.com>
2012-04-18 15:35:34 +0200

    db sync

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+﻿from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin, auth
