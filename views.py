9e5f14a1afcf593d6793d935c9b7db21b0d7c991
Paweł Tobiszewski <tobikster@gmail.com>
2012-05-16 14:45:03 +0200

    Działa naukagit status git status  xD

--- views.py
+++ views.py
@@ -2,4 +2,6 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def index(request):
+	if 'word' in request.session:
+		print '"word" from index =', request.session['word']
 	return render_to_response('index.html', context_instance = RequestContext(request))
